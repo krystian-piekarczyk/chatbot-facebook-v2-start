e7f90c93f836cc32c3d2ef1a8e84afa8a6bb2fa4
Piekarczyk <UJ30PL@pl.ing-ad>
2019-05-17 12:26:02 +0200

    przycisk

--- app.js
+++ app.js
@@ -202,7 +202,7 @@ function handleEcho(messageId, appId, metadata) {
     console.log("Received echo for message %s and app %d with metadata %s", messageId, appId, metadata);
 }
 
-function handleDialogFlowAction(sender, action, messages, contexts, parameters) {
+function handleDialogFlowAction(sender, action, messages, contexts, parameters,quickReplies) {
     switch (action) {
 
         case "Facebook_Location": //quick replies
